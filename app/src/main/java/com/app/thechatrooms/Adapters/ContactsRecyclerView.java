7393711be691d03d930ef7f1f332f58084bfa58e
kshitijshah <shahksh1011@gmail.com>
2019-09-04 16:44:07 -0400

    Contact List Created!

---
+++ app/src/main/java/com/app/thechatrooms/Adapters/ContactsRecyclerView.java
@@ -0,0 +1,68 @@
+package com.app.thechatrooms.Adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.app.thechatrooms.R;
+import com.app.thechatrooms.models.User;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+
+public class ContactsRecyclerView extends RecyclerView.Adapter<ContactsRecyclerView.ViewHolder> {
+    Context context;
+    FirebaseStorage storage;
+    FirebaseDatabase dbRef;
+    ArrayList<User> userList;
+
+    public ContactsRecyclerView(ArrayList<User> userList, Activity a, Context context){
+        Log.d("test", userList.get(0).getCity());
+        Log.d("test", "*****************************8");
+        this.userList = userList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_contacts_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        storage = FirebaseStorage.getInstance();
+        dbRef = FirebaseDatabase.getInstance();
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final User user = userList.get(position);
+        holder.contactName.setText(user.getFirstName()+ " " +user.getLastName());
+        Uri myUri = user.getUserProfileImage();
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView contactName ;
+        ViewHolder(View itemView){
+            super(itemView);
+            contactName = itemView.findViewById(R.id.fragment_contacts_item_userName);
+        }
+
+    }
+}
--- app/src/main/java/com/app/thechatrooms/ui/contacts/ContactsFragment.java
+++ app/src/main/java/com/app/thechatrooms/ui/contacts/ContactsFragment.java
@@ -1,6 +1,7 @@
 package com.app.thechatrooms.ui.contacts;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,18 +12,62 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.app.thechatrooms.Adapters.ContactsRecyclerView;
 import com.app.thechatrooms.R;
+import com.app.thechatrooms.models.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 public class ContactsFragment extends Fragment {
 
     private ContactsViewModel contactsViewModel;
+    StorageReference storageReference;
+    private DatabaseReference myRef;
+    private FirebaseDatabase firebaseDatabase;
+    ArrayList<User> userList = new ArrayList<>();
 
+    ContactsRecyclerView contactsRecyclerView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         contactsViewModel =
                 ViewModelProviders.of(this).get(ContactsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_contacts, container, false);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = firebaseDatabase.getReference("chatRooms/userProfiles");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userList.clear();
+                for(DataSnapshot child :  dataSnapshot.getChildren()){
+                    Log.d("CHILD", child.toString());
+                    User user = child.getValue(User.class);
+                    userList.add(user);
+                    RecyclerView recyclerView = root.findViewById(R.id.fragment_contacts_recyclerView);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                    contactsRecyclerView = new ContactsRecyclerView(userList, getActivity(),getContext());
+                    recyclerView.setAdapter(contactsRecyclerView);
+                    contactsRecyclerView.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
         final TextView textView = root.findViewById(R.id.text_slideshow);
         contactsViewModel.getText().observe(this, new Observer<String>() {
             @Override
